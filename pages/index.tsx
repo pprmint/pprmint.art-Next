21d79ef9bbfc76779a5fbee63eb51b7649cdfe87
pprmint <29925944+pprmint@users.noreply.github.com>
2022-07-31 19:51:19 +0200

    Test whether OpenGraph works for individual pages.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Head from "next/head";
 import type { NextPage } from "next";
 import { Container, Typography, Box } from "@mui/material";
 import Link from "../src/Link";
@@ -7,26 +8,48 @@ import Copyright from "../src/Copyright";
 
 const Home: NextPage = () => {
 	return (
-		<Container maxWidth="lg">
-			<Box
-				sx={{
-					my: 4,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<Typography variant="h4" component="h1" gutterBottom>
-					MUI v5 + Next.js with TypeScript example
-				</Typography>
-				<Link href="/works/2022/lights" color="secondary">
-					Go to the about page
-				</Link>
-				<ProTip />
-				<Copyright />
-			</Box>
-		</Container>
+		<>
+			<Head>
+				<title>pprmint.art</title>
+				<meta
+					name="description"
+					content="I make vector points, polygons, keyframes and colorful monospace letters look neat."
+				/>
+				<meta name="theme-color" content="#00cc66" />
+				<meta
+					property="og:description"
+					content="I make vector points, polygons, keyframes and colorful monospace letters look neat."
+				/>
+				<meta
+					property="og:image"
+					content="https://next.pprmint.art/og/index.png"
+				/>
+				<meta property="og:image:width" content="1920" />
+				<meta property="og:image:height" content="1080" />
+				<meta property="og:title" content="A Next.JS site." />
+				<meta property="og:url" content="https://next.pprmint.art" />
+			</Head>
+			<Container maxWidth="lg">
+				<Box
+					sx={{
+						my: 4,
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<Typography variant="h4" component="h1" gutterBottom>
+						MUI v5 + Next.js with TypeScript example
+					</Typography>
+					<Link href="/works/2022/lights" color="secondary">
+						Go to the about page
+					</Link>
+					<ProTip />
+					<Copyright />
+				</Box>
+			</Container>
+		</>
 	);
 };
 
--- pages/works/2022/lights.tsx
+++ pages/works/2022/lights.tsx
@@ -1,10 +1,33 @@
+import Head from "next/head";
 import { Container, Typography, Box } from "@mui/material";
 
 export default function Lights() {
 	return (
-		<Container maxWidth="lg">
-			<Typography variant="h1top">downloads</Typography>
-			<Typography variant="h1">It's a thing!</Typography>
-		</Container>
+		<>
+			<Head>
+				<title>lights // pprmint.art</title>
+				<meta
+					name="description"
+					content="Holy shit if this works I swear to god."
+				/>
+				<meta name="theme-color" content="#1199ff" />
+				<meta
+					property="og:description"
+					content="Holy shit if this works I swear to god."
+				/>
+				<meta
+					property="og:image"
+					content="https://next.pprmint.art/og/noclickbait.png"
+				/>
+				<meta property="og:image:width" content="1920" />
+				<meta property="og:image:height" content="1080" />
+				<meta property="og:title" content="please work." />
+				<meta property="og:url" content="https://next.pprmint.art/works/2022/lights" />
+			</Head>
+			<Container maxWidth="lg">
+				<Typography variant="h1top">downloads</Typography>
+				<Typography variant="h1">It's a thing!</Typography>
+			</Container>
+		</>
 	);
 }
