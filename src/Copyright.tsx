780f67ac0429c31f0f6d5febe6b20d43e3e0d424
pprmint <29925944+pprmint@users.noreply.github.com>
2022-07-31 19:49:16 +0200

    Add link to finished website in footer.

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -4,12 +4,20 @@ import MuiLink from "@mui/material/Link";
 
 export default function Copyright() {
 	return (
-		<Typography variant="body2" color="text.secondary" align="center">
-			{"© "}
-            {new Date().getFullYear()}{" "}
-			<MuiLink color="inherit" href="https://mui.com/">
-				pprmint.
-			</MuiLink>
-		</Typography>
+		<>
+			<Typography variant="body2" color="text.secondary" align="center">
+				{"© "}
+				{new Date().getFullYear()}{" "}
+				<MuiLink color="inherit" href="https://mui.com/">
+					pprmint.
+				</MuiLink>
+			</Typography>
+			<Typography variant="body2" color="text.secondary" align="center">
+				Looking for a finished website?{" "}
+				<MuiLink color="inherit" href="https://pprmint.art/">
+					Click here.
+				</MuiLink>
+			</Typography>
+		</>
 	);
 }
