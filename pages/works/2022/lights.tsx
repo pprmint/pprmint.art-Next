3e8f95327df472afc8fe98410b99f1968918e71b
pprmint <29925944+pprmint@users.noreply.github.com>
2022-07-31 17:11:54 +0200

    a b

---
+++ pages/works/2022/lights.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography, Box } from "@mui/material";
+
+export default function Lights() {
+	return (
+		<Container maxWidth="lg">
+			<Typography variant="h1">
+				Heck yes.
+			</Typography>
+		</Container>
+	);
+}
