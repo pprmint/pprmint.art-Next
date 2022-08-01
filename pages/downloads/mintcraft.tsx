0f7d194e0880a64607188a1924067b96847dcd4c
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-01 15:11:45 +0200

    Fix favicon URL for Mintcraft page

--- pages/downloads/mintcraft.tsx
+++ pages/downloads/mintcraft.tsx
@@ -7,17 +7,16 @@ export default function Mintcraft() {
 			<Head>
 				<title>Mintcraft</title>
 
-                <link rel="apple-touch-icon" sizes="180x180" href="./favicons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="./favicons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="./favicons/favicon-16x16.png" />
-                <link rel="manifest" href="./favicons/site.webmanifest" />
-                <link rel="mask-icon" href="./favicons/safari-pinned-tab.svg" color="#ffbb22" />
-                <link rel="shortcut icon" href="./favicons/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/mintcraft/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/mintcraft/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/mintcraft/favicon-16x16.png" />
+                <link rel="manifest" href="/favicons/mintcraft/site.webmanifest" />
+                <link rel="mask-icon" href="/favicons/mintcraft/safari-pinned-tab.svg" color="#ffbb22" />
+                <link rel="shortcut icon" href="/favicons/mintcraft/favicon.ico" />
                 <meta name="msapplication-TileColor" content="#ffbb22" />
-                <meta name="msapplication-config" content="./favicons/browserconfig.xml" />
+                <meta name="msapplication-config" content="/favicons/mintcraft/browserconfig.xml" />
                 <meta name="theme-color" content="#ffbb22" />
 
-                <link rel="shortcut icon" href="/static/leaf.ico" />
 				<meta
 					name="description"
 					content="Holy shit if this works I swear to god."
