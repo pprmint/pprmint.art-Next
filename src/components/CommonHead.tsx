6c0439f1b1cabb73394d24010007184409558895
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-07 18:12:34 +0200

    Fix title element receiving array with >1 children.

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 export default function CommonHead(props: {
 	title: string;
@@ -8,32 +8,64 @@ export default function CommonHead(props: {
 	favicon?: string; // Must match respective folder name inside /public/favicons.
 	color?: string; // Can't use short HEX codes (#123), must be #123456
 }) {
-    // Get URL for OpenGraph metadata.
-    const router = useRouter();
-    // Default props in case these props aren't passed.
-    const { favicon = "pprmint" } = props;
-    const { color = "#00cc66" } = props;
+	// Output title in browser as single node.
+	const title = `${props.title} // pprmint.art`;
+	// Get URL for OpenGraph metadata.
+	const router = useRouter();
+	// Default props if not passed in the element.
+	const { favicon = "pprmint" } = props;
+	const { color = "#00cc66" } = props;
 	return (
 		<Head>
-            {/* Basic metadata */}
-			<title>{props.title} // pprmint.art</title>
+			{/* Basic metadata */}
+			<title>{title}</title>
 			<meta name="description" content={props.description} />
 			<meta name="theme-color" content={color} />
 
-            {/* Favicons and other things for different platforms */}
-			<link rel="apple-touch-icon" sizes="180x180" href={"../favicons/"+favicon+"/apple-touch-icon.png"} />
-			<link rel="icon" type="image/png" sizes="32x32" href={"../favicons/"+favicon+"/favicon-32x32.png"} />
-			<link rel="icon" type="image/png" sizes="16x16" href={"../favicons/"+favicon+"/favicon-16x16.png"} />
-			<link rel="manifest" href={"../favicons/"+favicon+"/site.webmanifest"} />
-			<link rel="mask-icon" href={"../favicons/"+favicon+"/safari-pinnedtab.svg"} color={color} />
-			<link rel="shortcut icon" href={"../favicons/"+favicon+"/favicon.ico"}/>
+			{/* Favicons and other things for different platforms */}
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href={"../favicons/" + favicon + "/apple-touch-icon.png"}
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href={"../favicons/" + favicon + "/favicon-32x32.png"}
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href={"../favicons/" + favicon + "/favicon-16x16.png"}
+			/>
+			<link
+				rel="manifest"
+				href={"../favicons/" + favicon + "/site.webmanifest"}
+			/>
+			<link
+				rel="mask-icon"
+				href={"../favicons/" + favicon + "/safari-pinnedtab.svg"}
+				color={color}
+			/>
+			<link
+				rel="shortcut icon"
+				href={"../favicons/" + favicon + "/favicon.ico"}
+			/>
 
-            {/* OpenGraph metadata */}
+			{/* OpenGraph metadata */}
 			<meta content="summary_large_image" name="twitter:card" />
 			<meta property="og:description" content={props.description} />
-			<meta property="og:image" content={"https://next.pprmint.art/og/"+props.ogImg} />
+			<meta
+				property="og:image"
+				content={"https://next.pprmint.art/og/" + props.ogImg}
+			/>
 			<meta property="og:title" content={props.title} />
-			<meta property="og:url" content={"https://next.pprmint.art"+router.pathname} />
+			<meta
+				property="og:url"
+				content={"https://next.pprmint.art" + router.pathname}
+			/>
 		</Head>
 	);
 }
