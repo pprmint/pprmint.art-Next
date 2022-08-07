9f7d65bfd0a9d33bda0a7dec64f0d6969b73ed2d
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-07 22:26:22 +0200

    Replace Noway and N27 with Silka and Basier Circle.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,19 @@ import Head from "next/head";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import createEmotionCache from "../src/createEmotionCache";
+import createEmotionCache from "src/createEmotionCache";
 
-import "../fonts/Noway/Noway.css";
-import "../fonts/N27/N27.css";
+import "fonts/Silka/silka.css";
+import "fonts/BasierCircle/basier_circle.css";
+import "fonts/BasierCircleMono/basier_circle_mono.css";
 
-import theme from "../src/theme";
-import "../src/global.scss";
+import theme from "src/theme";
+import "src/global.scss";
 
-import Navigation from "../src/components/Navigation";
+import Navigation from "src/components/Navigation";
+import Copyright from "src/components/Copyright";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -38,20 +39,12 @@ export default function MyApp(props: MyAppProps) {
 					content="initial-scale=1, width=device-width"
 				/>
 			</Head>
-            <Navigation />
 			<ThemeProvider theme={theme}>
 				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
-				<AnimatePresence
-					exitBeforeEnter
-					// onExitComplete={() => window.scrollTo(0, 0)}
-				>
-					<Component
-						{...pageProps}
-						location={location}
-						key={location.pathname}
-					/>
-				</AnimatePresence>
+				<Navigation />
+				<Component {...pageProps} />
+				<Copyright />
 			</ThemeProvider>
 		</CacheProvider>
 	);
