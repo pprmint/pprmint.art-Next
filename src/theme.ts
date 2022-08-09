7962ab8c6c7fa603552e427258ff31a78bd8c401
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-09 11:50:21 +0000

    Text link underline transition same as for buttons.

--- src/theme.ts
+++ src/theme.ts
@@ -66,6 +66,7 @@ let theme = createTheme({
         MuiLink: {
             styleOverrides: {
                 root: {
+                    transition: "250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
                     fontWeight: 600,
                     textDecorationThickness: 2,
                 },
