0dc34e94175db9194e26653ad33d4fdc17096bf4
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-13 01:49:29 +0200

    funny

--- pages/project/mintcraft.tsx
+++ pages/project/mintcraft.tsx
@@ -16,6 +16,7 @@ import Link from "src/components/Link";
 import Footer from "src/components/Footer";
 import Image from "next/image";
 import { FiExternalLink } from "react-icons/fi";
+import { Parallax } from "react-scroll-parallax";
 
 // To be added: Function to reuse ribbons with texts as children
 
@@ -35,78 +36,99 @@ export default function Mintcraft() {
 				color="#ffbb22"
 			/>
 			<Title top={t("Title.top")} bottom={t("Title.bottom")} />
-			<Container
-				maxWidth={false}
-				sx={{
-					minHeight: 500,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					backgroundImage: "linear-gradient(to bottom, #1110, #111c), url(/assets/mintcraft/furnace_bg.png)",
-					backgroundRepeat: "no-repeat",
-					backgroundPosition: "center top",
-                    backgroundSize: "cover",
-				}}
-			>
-				<Container sx={{ textAlign: "center" }}>
-					<Typography variant="h2">
-						{t("Content.About.Dark.heading")}
-					</Typography>
-					<Typography>
-						{t("Content.About.Dark.text1")}
-						<br />
-						{t.rich("Content.About.Dark.text2", {
-							strong: (children) => <strong>{children}</strong>,
-						})}
-						<br />
-						<Link
-							className="external"
-							color="secondary"
-							href="https://vanillatweaks.net/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Vanilla Tweaks
-							<FiExternalLink />
-						</Link>
-					</Typography>
-				</Container>
+			<Container maxWidth="xl" sx={{ minHeight: "75vh" }}>
+				<Grid container spacing={8}>
+					<Grid
+						item
+						xs={12}
+						md={8}
+						sx={{
+							textAlign: {
+								md: "left",
+								xs: "center",
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "center",
+							},
+						}}
+					>
+						<Typography variant="h2">
+							{t("Content.About.Dark.heading")}
+						</Typography>
+						<Typography>
+							{t("Content.About.Dark.text1")}
+							<br />
+							{t.rich("Content.About.Dark.text2", {
+								strong: (children) => <strong>{children}</strong>,
+							})}
+							<br />
+							<Link
+								className="external"
+								color="secondary"
+								href="https://vanillatweaks.net/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Vanilla Tweaks
+								<FiExternalLink />
+							</Link>
+						</Typography>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Parallax speed={10} rotate={[-10, 10]} easing="easeInQuad">
+							<Image
+								src="/assets/mintcraft/furnace.png"
+								width="500px"
+								height="500px"
+							/>
+						</Parallax>
+					</Grid>
+				</Grid>
 			</Container>
-			<Container
-				maxWidth={false}
-				sx={{
-					minHeight: 500,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					backgroundImage: "linear-gradient(to bottom, #1110, #111c), url(/assets/mintcraft/beacon_bg.png)",
-					backgroundRepeat: "no-repeat",
-					backgroundPosition: "center top",
-                    backgroundSize: "cover",
-				}}
-			>
-				<Container sx={{ textAlign: "center" }}>
-					<Typography variant="h2">
-						{t("Content.About.Consistent.heading")}
-					</Typography>
-					<Typography>{t("Content.About.Consistent.text")}</Typography>
-				</Container>
+			<Container maxWidth="xl" sx={{ minHeight: "75vh" }}>
+				<Grid container spacing={8}>
+					<Grid
+						item
+						xs={12}
+						md={8}
+						sx={{
+							textAlign: {
+								md: "left",
+								xs: "center",
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "center",
+							},
+						}}
+					>
+						<Typography variant="h2">
+							{t("Content.About.Consistent.heading")}
+						</Typography>
+						<Typography>{t("Content.About.Consistent.text")}</Typography>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Parallax speed={10} rotate={[10, -10]} easing="easeInQuad">
+							<Image
+								src="/assets/mintcraft/beacon.png"
+								width="500px"
+								height="500px"
+							/>
+						</Parallax>
+					</Grid>
+				</Grid>
 			</Container>
-			<Container
-				maxWidth={false}
-				sx={{
-					minHeight: 500,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					textAlign: "center",
-					backgroundImage: "linear-gradient(to bottom, #1110, #111c), url(/assets/mintcraft/inventory_bg.png)",
-					backgroundRepeat: "no-repeat",
-					backgroundPosition: "center top",
-                    backgroundSize: "cover",
-				}}
-			>
-				<Container sx={{ textAlign: "center" }}>
+			<Container maxWidth="xl" sx={{ minHeight: "75vh" }}>
+				<Box
+					sx={{
+						textAlign: {
+							md: "left",
+							xs: "center",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+						},
+					}}
+				>
 					<Typography variant="h2">
 						{t("Content.About.Modern.heading")}
 					</Typography>
@@ -117,7 +139,14 @@ export default function Mintcraft() {
 							),
 						})}
 					</Typography>
-				</Container>
+					<Parallax speed={10} rotate={[-10, 10]} easing="easeInQuad">
+						<Image
+							src="/assets/mintcraft/inventory.png"
+							width="1000px"
+							height="500px"
+						/>
+					</Parallax>
+				</Box>
 			</Container>
 			<Footer />
 		</motion.div>
