3e6e9aa7855c5ecd620bb268dab5619459d5bfb6
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-12 12:48:38 +0000

    Fancier titles with parallax effect

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,5 @@
 import { Typography, Container, Box } from "@mui/material";
-import { Parallax } from "react-scroll-parallax";
-import styles from "./title.module.scss";
+import { Parallax, ParallaxBanner } from "react-scroll-parallax";
 
 export default function Title(
 	props: React.PropsWithChildren<{
@@ -13,6 +12,63 @@ export default function Title(
 	}>
 ) {
 	if (props.big) {
+		return (
+			<ParallaxBanner
+				layers={[
+					{
+						image: props.src,
+						speed: -20,
+						id: "background",
+						opacity: [0.8, 0.3],
+					},
+				]}
+				style={{
+					minHeight: 500,
+					marginBottom: 64,
+					display: "flex",
+					alignItems: "center",
+					overflow: "hidden",
+				}}
+			>
+				<Container maxWidth="xl">
+					<Box
+						sx={{
+							mb: 8,
+                            pt: 6,
+							animation: "shiftFromRight 1s var(--easeOut) forwards",
+						}}
+					>
+						<Parallax speed={-10}>
+							<Typography
+								variant="h1top"
+								color="text.secondary"
+								sx={{ textShadow: "0 4px 10px #1115" }}
+							>
+								{props.top}
+							</Typography>
+							<Typography
+								variant="h1"
+								gutterBottom
+								sx={{
+									animation: "shiftFromRight 1s var(--easeOut) forwards",
+									textShadow: "0 4px 10px #1115",
+								}}
+							>
+								{props.bottom}
+							</Typography>
+							<Typography variant="body1" gutterBottom>
+								{props.body}
+							</Typography>
+							<Box sx={{ display: "flex", justifyContent: "right" }}>
+								{props.children}
+							</Box>
+						</Parallax>
+					</Box>
+				</Container>
+			</ParallaxBanner>
+		);
+	} else {
+		/* if (props.big) {
 		return (
 			<Container
 				className={styles.titleContainer}
@@ -48,24 +104,30 @@ export default function Title(
 				</Container>
 			</Container>
 		);
-	} else {
+	}*/
 		return (
 			<>
 				<Container
 					maxWidth={false}
 					sx={{
-                        mt: 10,
+						mt: 10,
 						py: 8,
 						maxWidth: "max-content",
 						display: "flex",
 						alignItems: "center",
 					}}
 				>
-					<Container sx={{animation: "shiftFromRight 1s var(--easeOut) forwards"}}>
+					<Container
+						sx={{ animation: "shiftFromRight 1s var(--easeOut) forwards" }}
+					>
 						<Typography variant="h1top" color="text.secondary">
 							{props.top}
 						</Typography>
-						<Typography variant="h1" gutterBottom sx={{animation: "shiftFromRight 1s var(--easeOut) forwards"}}>
+						<Typography
+							variant="h1"
+							gutterBottom
+							sx={{ animation: "shiftFromRight 1s var(--easeOut) forwards" }}
+						>
 							{props.bottom}
 						</Typography>
 					</Container>
