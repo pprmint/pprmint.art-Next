fa6cba1301464e3dbf34071ccaa411857ee7dca0
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-15 12:23:55 +0000

    Remove commented part.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -65,43 +65,6 @@ export default function Title(
 			</ParallaxBanner>
 		);
 	} else {
-		/* if (props.big) {
-		return (
-			<Container
-				className={styles.titleContainer}
-				maxWidth={false}
-				sx={{
-					display: "flex",
-					alignItems: "center",
-					overflow: "hidden",
-					height: 500,
-					mb: 8,
-				}}
-			>
-				{props.children}
-				<Container maxWidth="xl">
-					<Box
-						sx={{
-							textShadow: "0 4px 10px #1115",
-                            animation: "shiftFromRight 1s var(--easeOut) forwards"
-						}}
-					>
-						<Parallax speed={-8}>
-							<Typography variant="h1top" color="text.secondary">
-								{props.top}
-							</Typography>
-							<Typography variant="h1" gutterBottom style={{animation: "shiftFromRight 1s var(--easeOut) forwards"}}>
-								{props.bottom}
-							</Typography>
-							<Typography variant="body1" gutterBottom>
-								{props.body}
-							</Typography>
-						</Parallax>
-					</Box>
-				</Container>
-			</Container>
-		);
-	}*/
 		return (
 			<>
 				<Container
