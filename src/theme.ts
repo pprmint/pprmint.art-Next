f9a046e82ea776145078d1d2f5f97f4ff7d1d9e7
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-18 10:32:30 +0000

    Tweak line height and border radii.

--- src/theme.ts
+++ src/theme.ts
@@ -2,107 +2,7 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 // Create a theme instance.
 let theme = createTheme({
-	typography: {
-		allVariants: {
-			lineHeight: 1.6,
-			color: "#eeeeeec4",
-			fontFamily: "'Basier Circle', Roboto, Helvetica, Arial, sans-serif",
-		},
-		h1: {
-			fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			color: "#eee",
-			fontWeight: "600",
-			lineHeight: "1",
-		},
-		h1top: {
-			fontWeight: "300",
-			fontStyle: "italic",
-			fontSize: "2rem",
-			lineHeight: "1",
-		},
-		h2: {
-            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			fontWeight: 500,
-			color: "#eee",
-		},
-		h3: {
-            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			fontWeight: 500,
-			color: "#eee",
-		},
-		h4: {
-            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			fontWeight: 500,
-			color: "#eee",
-		},
-		h5: {
-            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			fontWeight: 500,
-			color: "#eee",
-		},
-		h6: {
-            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
-			fontWeight: 500,
-			color: "#eee",
-		},
-	},
-	components: {
-		MuiTypography: {
-			styleOverrides: {
-				gutterBottom: {
-					marginBottom: 18,
-				},
-			},
-		},
-        MuiPaper: {
-			styleOverrides: {
-				elevation0: {
-					background: "var(--backgroundPrimary)",
-				},
-				elevation1: {
-					background: "var(--backgroundSecondary)",
-				},
-				elevation2: {
-					background: "var(--backgroundTertiary)",
-				},
-			},
-		},
-		MuiSkeleton: {
-			styleOverrides: {
-				root: {
-					backgroundColor: "transparent",
-				},
-			},
-		},
-		MuiButton: {
-			styleOverrides: {
-				outlined: {
-					borderWidth: "2px !important",
-				},
-			},
-		},
-		MuiToggleButton: {
-			styleOverrides: {
-				root: {
-					borderWidth: "2px !important",
-				},
-			},
-		},
-		MuiLink: {
-			styleOverrides: {
-				root: {
-					transition: "250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-					fontWeight: 600,
-					textDecorationThickness: 1,
-                    textUnderlineOffset: 2,
-					"&:hover": {
-						textDecorationThickness: 2,
-					},
-				},
-			},
-		},
-	},
-	palette: {
+    palette: {
 		mode: "dark",
 		background: {
 			default: "#111",
@@ -141,6 +41,124 @@ let theme = createTheme({
 			dark: "#b23",
 		},
 	},
+	typography: {
+        h1: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			color: "#eee",
+			fontWeight: "600",
+			lineHeight: "1",
+		},
+		h1top: {
+			fontWeight: "300",
+			fontStyle: "italic",
+			fontSize: "2rem",
+			lineHeight: "1",
+		},
+		h2: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			fontWeight: 500,
+			color: "#eee",
+		},
+		h3: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			fontWeight: 500,
+			color: "#eee",
+		},
+		h4: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			fontWeight: 500,
+			color: "#eee",
+		},
+		h5: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			fontWeight: 500,
+			color: "#eee",
+		},
+		h6: {
+            fontFamily: "Silka, Roboto, Helvetica, Arial, sans-serif",
+			fontWeight: 500,
+			color: "#eee",
+		},
+        body1: {
+            lineHeight: 1.6,
+            color: "#eeeeeec4",
+            fontFamily: "'Basier Circle', Roboto, Helvetica, Arial, sans-serif",
+        },
+        body2: {
+            lineHeight: 1.6,
+            color: "#eeeeeec4",
+            fontFamily: "'Basier Circle', Roboto, Helvetica, Arial, sans-serif",
+        },
+	},
+	components: {
+		MuiTypography: {
+			styleOverrides: {
+				gutterBottom: {
+					marginBottom: 18,
+				},
+			},
+		},
+        MuiPaper: {
+			styleOverrides: {
+				elevation0: {
+					background: "var(--backgroundPrimary)",
+				},
+				elevation1: {
+					background: "var(--backgroundSecondary)",
+				},
+				elevation2: {
+					background: "var(--backgroundTertiary)",
+				},
+			},
+		},
+		MuiSkeleton: {
+			styleOverrides: {
+				root: {
+					backgroundColor: "transparent",
+				},
+			},
+		},
+		MuiButton: {
+			styleOverrides: {
+				outlined: {
+					borderWidth: "2px !important",
+				},
+                sizeLarge: {
+                    borderRadius: 22,
+                },
+                sizeMedium: {
+                    borderRadius: 20,
+                },
+                sizeSmall: {
+                    paddingRight: 16,
+                    paddingLeft: 16,
+                },
+			},
+		},
+		MuiToggleButton: {
+			styleOverrides: {
+				root: {
+					borderWidth: "2px !important",
+				},
+			},
+		},
+		MuiLink: {
+			styleOverrides: {
+				root: {
+					transition: "250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+					fontWeight: 600,
+					textDecorationThickness: 1,
+                    textUnderlineOffset: 2,
+					"&:hover": {
+						textDecorationThickness: 2,
+					},
+				},
+			},
+		},
+	},
+    shape: {
+        borderRadius: 8,
+    },
 });
 
 declare module "@mui/material/styles" {
