4ddc4d7cfa132c5eb6e52bfdcd4f1f5e10e9a719
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-23 12:58:43 +0000

    Get out of my sight.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -141,7 +141,7 @@ export default function Navigation() {
                     ))}
                 </BottomNavigation>
             </Box>
-            <ScrollTop />
+            {/* <ScrollTop />*/}
         </>
     );
 }
