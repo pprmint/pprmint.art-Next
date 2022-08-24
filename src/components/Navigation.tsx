a97d57b56ab0787325ccdb36fc3ba621284d9422
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-24 08:33:53 +0000

    By our goddess and saviour Hatsune Miku, how the fuck are the PCs we use at work so fucking slow holy shit which fucker approved this pile of e-waste.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import { GetStaticPropsContext } from "next";
 import { Router, useRouter } from "next/router";
 import {
-	AppBar,
-	Fab,
-	Box,
-	useScrollTrigger,
-	Zoom,
-	BottomNavigation,
-	BottomNavigationAction,
-	IconButton,
-	Paper,
-	Stack,
-	Tooltip,
-	Toolbar,
+    AppBar,
+    Fab,
+    Box,
+    useScrollTrigger,
+    Zoom,
+    BottomNavigation,
+    BottomNavigationAction,
+    IconButton,
+    Paper,
+    Stack,
+    Tooltip,
+    Toolbar,
 } from "@mui/material";
 import Lottie from "react-lottie-player";
 import wordmarkJson from "src/animations/wordmark.json";
@@ -22,155 +22,180 @@ import { NextLinkComposed } from "src/components/Link";
 import Link from "src/components/Link";
 
 import {
-	FiChevronUp,
-	FiHome,
-	FiImage,
-	FiPackage,
-	FiMail,
+    FiChevronUp,
+    FiHome,
+    FiImage,
+    FiPackage,
+    FiMail,
 } from "react-icons/fi";
 import {
-	HomeOutlined,
-	HomeTwoTone,
-	ImageOutlined,
-	ImageTwoTone,
-	FolderZipOutlined,
-	FolderZipTwoTone,
-	MailOutlined,
-	MailTwoTone,
+    HomeOutlined,
+    HomeTwoTone,
+    ImageOutlined,
+    ImageTwoTone,
+    FolderZipOutlined,
+    FolderZipTwoTone,
+    MailOutlined,
+    MailTwoTone,
 } from "@mui/icons-material";
 
 const Links = [
-	{
-		name: "Home",
-		path: "/",
-		outlinedIcon: <HomeOutlined />,
-		filledIcon: <HomeTwoTone />,
-	},
-	{
-		name: "Gallery",
-		path: "/gallery",
-		outlinedIcon: <ImageOutlined />,
-		filledIcon: <ImageTwoTone />,
-	},
-	{
-		name: "Projects",
-		path: "/projects",
-		outlinedIcon: <FolderZipOutlined />,
-		filledIcon: <FolderZipTwoTone />,
-	},
-	{
-		name: "Contact",
-		path: "/contact",
-		outlinedIcon: <MailOutlined />,
-		filledIcon: <MailTwoTone />,
-	},
+    {
+        name: "Home",
+        path: "/",
+        outlinedIcon: <HomeOutlined />,
+        filledIcon: <HomeTwoTone />,
+    },
+    {
+        name: "Gallery",
+        path: "/gallery",
+        outlinedIcon: <ImageOutlined />,
+        filledIcon: <ImageTwoTone />,
+    },
+    {
+        name: "Projects",
+        path: "/projects",
+        outlinedIcon: <FolderZipOutlined />,
+        filledIcon: <FolderZipTwoTone />,
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+        outlinedIcon: <MailOutlined />,
+        filledIcon: <MailTwoTone />,
+    },
 ];
 
 function ScrollTop() {
-	const trigger = useScrollTrigger({
-		disableHysteresis: true,
-		threshold: 75,
-	});
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 75,
+    });
 
-	return (
-		<Zoom in={trigger}>
-			<Box
-				onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-				sx={{ position: "fixed", bottom: 28, right: 28, zIndex: 9999 }}
-			>
-				<Fab size="large" aria-label="scroll back to top">
-					<FiChevronUp />
-				</Fab>
-			</Box>
-		</Zoom>
-	);
+    return (
+        <Zoom in={trigger}>
+            <Box
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                sx={{ position: "fixed", bottom: 28, right: 28, zIndex: 9999 }}
+            >
+                <Fab size="large" aria-label="scroll back to top">
+                    <FiChevronUp />
+                </Fab>
+            </Box>
+        </Zoom>
+    );
 }
 
 export default function Navigation() {
-	const router = useRouter();
-	return (
-		<>
-			{/* Desktop navigation */}
-			<AppBar
-				color="transparent"
-				elevation={0}
-				sx={{
-					backgroundImage: "linear-gradient(#111f, #1110)",
-				}}
-			>
-				<Toolbar>
-					<Box sx={{ flexGrow: { xs: 1, sm: 0 } }}>
-						<Link href="/" scroll={false}>
-							<Lottie
-								loop={false}
-								animationData={wordmarkJson}
-								play
-								style={{ height: 75, width: "min-width" }}
-							/>
-						</Link>
-					</Box>
-					{/* Box to extend buttons to the right side. */}
-					<Box
-						sx={{
-							display: { xs: "none", sm: "flex" },
-							flexGrow: 1,
-						}}
-					/>
-					<Box sx={{ display: { xs: "none", sm: "flex" } }}>
-						<Stack spacing={1} direction="row">
-							{Links.map((link) => (
-								<IconButton
-									component={Link}
-									href={link.path}
-									scroll={false}
-									aria-label={link.name}
-									key={link.path}
-								>
-									{link.outlinedIcon}
-								</IconButton>
-							))}
-						</Stack>
-					</Box>
-				</Toolbar>
-			</AppBar>
-			{/* Mobile navigation */}
-			<Box
-				sx={{
-					backgroundColor: "var(--backgroundSecondary)",
-					display: { xs: "static", sm: "none" },
-					position: "fixed",
-					bottom: "0",
-					width: "100%",
-					zIndex: 9999,
-				}}
-			>
-				<BottomNavigation value={router.pathname}>
-					{Links.map((link) => (
-						<BottomNavigationAction
-							component={NextLinkComposed}
-							key={link.path}
-							to={link.path}
+    const router = useRouter();
+    return (
+        <>
+            {/* Desktop navigation */}
+            <AppBar
+                color="transparent"
+                elevation={0}
+                sx={{
+                    backgroundImage: "linear-gradient(#111f, #1110)",
+                }}
+            >
+                <Toolbar>
+                    <Box sx={{ flexGrow: { xs: 1, sm: 0 } }}>
+                        <Link href="/" scroll={false}>
+                            <Lottie
+                                loop={false}
+                                animationData={wordmarkJson}
+                                play
+                                style={{ height: 75, width: "min-width" }}
+                            />
+                        </Link>
+                    </Box>
+                    {/* Box to extend buttons to the right side. */}
+                    <Box
+                        sx={{
+                            display: { xs: "none", sm: "flex" },
+                            flexGrow: 1,
+                        }}
+                    />
+                    <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+                        <Stack spacing={1} direction="row">
+                            {Links.map((link) => (
+                                <IconButton
+                                    color={
+                                        router.pathname === link.path
+                                            ? "primary"
+                                            : "inherit"
+                                    }
+                                    component={Link}
+                                    href={link.path}
+                                    scroll={false}
+                                    aria-label={link.name}
+                                    key={link.path}
+                                    sx={{ borderRadius: 50 }}
+                                >
+                                    {router.pathname === link.path
+                                        ? link.filledIcon
+                                        : link.outlinedIcon}
+                                    <span
+                                        style={{
+                                            fontSize: "1.1rem",
+                                            lineHeight: 0,
+                                            marginLeft: 8,
+                                            color: "#eee",
+                                            display:
+                                                router.pathname === link.path
+                                                    ? "block"
+                                                    : "none",
+                                            transform: router.pathname === link.path
+                                            ? "translateX(0px)"
+                                            : "translateX(-10px)",
+                                        }}
+                                    >
+                                        {link.name}
+                                    </span>
+                                </IconButton>
+                            ))}
+                        </Stack>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {/* Mobile navigation */}
+            <Box
+                sx={{
+                    backgroundColor: "var(--backgroundSecondary)",
+                    display: { xs: "static", sm: "none" },
+                    position: "fixed",
+                    bottom: "0",
+                    width: "100%",
+                    zIndex: 9999,
+                }}
+            >
+                <BottomNavigation value={router.pathname}>
+                    {Links.map((link) => (
+                        <BottomNavigationAction
+                            component={NextLinkComposed}
+                            key={link.path}
+                            to={link.path}
                             scroll={false}
-							label={link.name}
-							icon={
-								router.pathname === link.path
-									? link.filledIcon
-									: link.outlinedIcon
-							}
-							value={link.path}
-						/>
-					))}
-				</BottomNavigation>
-			</Box>
-			{/* <ScrollTop />*/}
-		</>
-	);
+                            label={link.name}
+                            icon={
+                                router.pathname === link.path
+                                    ? link.filledIcon
+                                    : link.outlinedIcon
+                            }
+                            value={link.path}
+                        />
+                    ))}
+                </BottomNavigation>
+            </Box>
+            {/* <ScrollTop />*/}
+        </>
+    );
 }
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
-	return {
-		props: {
-			messages: (await import(`locales/${locale}/strings.json`)).default,
-		},
-	};
+    return {
+        props: {
+            messages: (await import(`locales/${locale}/strings.json`)).default,
+        },
+    };
 }
