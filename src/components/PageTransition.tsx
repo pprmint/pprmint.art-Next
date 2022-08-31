fe7474aa2f5f5426d2d2933584d66b92f8ba2455
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-31 21:59:39 +0200

    Cool transition.

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -6,18 +6,21 @@ export default function PageTransition(props: React.PropsWithChildren) {
 	return (
 		<motion.div
 			initial={{
-				opacity: 0,
-				// y: 20,
+                opacity: 0,
+				x: 40,
+				clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)",
 			}}
 			animate={{
                 opacity: 1,
-                // y: 0,
+                x: 0,
+				clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
 				transition: { duration: 0.5, ease: "circOut" },
 			}}
 			exit={{
-				opacity: 0,
-				// y: -20,
-				transition: { duration: 0.195, ease: "circIn" },
+                opacity: 0,
+				x: -20,
+				clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+				transition: { duration: 0.25, ease: "easeIn" },
 			}}
 		>
 			{props.children}
