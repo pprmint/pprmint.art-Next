c0c23fea1204e9ec4f8b0523001e0291162df31f
pprmint <29925944+pprmint@users.noreply.github.com>
2022-08-31 22:55:58 +0200

    Cool parallax thingies.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { GetStaticPropsContext } from "next";
 import { useTranslations } from "next-intl";
 import { Container, Box, Button, Typography } from "@mui/material";
+import { Parallax, ParallaxBanner } from "react-scroll-parallax";
 import { motion } from "framer-motion";
 
 import PageTransition from "src/components/PageTransition";
@@ -14,121 +15,140 @@ import { FiExternalLink } from "react-icons/fi";
 import { SiGithub } from "react-icons/si";
 
 export default function Home() {
-    const t = useTranslations("Home");
-    return (
-        <PageTransition>
-            <Head
-                title={t("Head.title")}
-                description={t("Head.description")}
-                ogImg="index.png"
-            />
-            <Title
-                big
-                top={t("Title.top")}
-                bottom={t("Title.bottom")}
-                body={t("Title.description")}
-                src="https://media.pprmint.art/code.png"
-            >
-                <Button
-                    variant="contained"
-                    component={Link}
-                    size="large"
-                    color="secondary"
-                    href="https://github.com/pprmint/pprmint.art-Next"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    startIcon={<SiGithub />}
-                    endIcon={<FiExternalLink />}
-                >
-                    {t("Title.button")}
-                </Button>
-            </Title>
-            <Box
-                sx={{
-                    mb: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <Button
-                    variant="outlined"
-                    component={Link}
-                    scroll={false}
-                    noLinkStyle
-                    href="/projects/mintcraft"
-                >
-                    Go to Mintcraft page
-                </Button>
-            </Box>
-            <Container maxWidth="lg">
-                <Typography>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                    aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                    sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                    nonumy eirmod tempor invidunt ut labore et dolore magna
-                    aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                    sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                    nonumy eirmod tempor invidunt ut labore et dolore magna
-                    aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                    sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </Typography>
-                <br />
-                <Typography variant="h2">Lorem ipsum.</Typography>
-                <Typography>
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate
-                    velit esse molestie consequat, vel illum dolore eu feugiat
-                    nulla facilisis at vero eros et accumsan et iusto odio
-                    dignissim qui blandit praesent luptatum zzril delenit augue
-                    duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
-                    amet, consectetuer adipiscing elit, sed diam nonummy nibh
-                    euismod tincidunt ut laoreet dolore magna aliquam erat
-                    volutpat.
-                </Typography>
-                <br />
-                <Typography variant="h2">Lorem ipsum.</Typography>
-                <Typography>
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate
-                    velit esse molestie consequat, vel illum dolore eu feugiat
-                    nulla facilisis.
-                </Typography>
-                <br />
-                <Typography variant="h2">Lorem ipsum.</Typography>
-                <Typography>
-                    At vero eos et accusam et justo duo dolores et ea rebum.
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At
-                    vero eos et accusam et justo duo dolores et ea rebum. Stet
-                    clita kasd gubergren, no sea takimata sanctus est Lorem
-                    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-                    sadipscing elitr, At accusam aliquyam diam diam dolore
-                    dolores duo eirmod eos erat, et nonumy sed tempor et et
-                    invidunt justo labore Stet clita ea et gubergren, kasd magna
-                    no rebum. sanctus sea sed takimata ut vero voluptua. est
-                    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                    invidunt ut labore et dolore magna aliquyam erat.
-                </Typography>
-                <br />
-            </Container>
-            <Footer />
-        </PageTransition>
-    );
+	const t = useTranslations("Home");
+	return (
+		<PageTransition>
+			<Head
+				title={t("Head.title")}
+				description={t("Head.description")}
+				ogImg="index.png"
+			/>
+			<Title
+				big
+				top={t("Title.top")}
+				bottom={t("Title.bottom")}
+				body={t("Title.description")}
+				src="https://media.pprmint.art/code.png"
+			>
+				<Button
+					variant="contained"
+					component={Link}
+					size="large"
+					color="secondary"
+					href="https://github.com/pprmint/pprmint.art-Next"
+					target="_blank"
+					rel="noopener noreferrer"
+					startIcon={<SiGithub />}
+					endIcon={<FiExternalLink />}
+				>
+					{t("Title.button")}
+				</Button>
+			</Title>
+			<Container maxWidth="lg">
+				<Typography variant="h2">{t("Content.About.heading")}</Typography>
+				<Typography variant="body1">{t("Content.About.text")}</Typography>
+			</Container>
+			<br />
+			<br />
+			<ParallaxBanner
+				layers={[
+					{
+						image: "/assets/home/motion/tracks.svg",
+					},
+					{
+						image: "/assets/home/motion/diamond.svg",
+						speed: -10,
+					},
+					{
+						image: "/assets/home/motion/whateverthefuckthisis.svg",
+						speed: 20,
+					},
+					{
+						image: "/assets/home/motion/rectangle.svg",
+						speed: 10,
+					},
+					{
+						image: "/assets/home/motion/circle.svg",
+						speed: -20,
+					},
+					{
+						image: "/assets/home/gradient.svg",
+					},
+				]}
+				style={{
+					minHeight: 600,
+					marginBottom: 64,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Box textAlign="center" zIndex={1}>
+					<Typography variant="h2">{t("Content.Motion.heading")}</Typography>
+					<Typography variant="body1">{t("Content.Motion.text")}</Typography>
+				</Box>
+			</ParallaxBanner>
+			<ParallaxBanner
+				layers={[
+					{
+						image: "/assets/home/code/tags.svg",
+						speed: -20,
+						rotate: [-10, 10],
+					},
+					{
+						image: "/assets/home/code/brackets.svg",
+						speed: -20,
+						rotate: [10, -10],
+					},
+					{
+						image: "/assets/home/gradient.svg",
+					},
+				]}
+				style={{
+					minHeight: 600,
+					marginBottom: 64,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Box textAlign="center" zIndex={1}>
+					<Typography variant="h2">{t("Content.Code.heading")}</Typography>
+					<Typography variant="body1">{t("Content.Code.text")}</Typography>
+				</Box>
+			</ParallaxBanner>
+			<ParallaxBanner
+				layers={[
+					{
+						image: "/assets/home/gradient.svg",
+					},
+				]}
+				style={{
+					minHeight: 600,
+					marginBottom: 64,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Box textAlign="center" zIndex={1}>
+					<Typography variant="h2">
+						{t("Content.Illustration.heading")}
+					</Typography>
+					<Typography variant="body1">
+						{t("Content.Illustration.text")}
+					</Typography>
+				</Box>
+			</ParallaxBanner>
+			<Footer />
+		</PageTransition>
+	);
 }
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
-    return {
-        props: {
-            messages: (await import(`locales/${locale}/strings.json`)).default,
-        },
-    };
+	return {
+		props: {
+			messages: (await import(`locales/${locale}/strings.json`)).default,
+		},
+	};
 }
