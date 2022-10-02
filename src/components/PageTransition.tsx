322b121aba6764ec81b0ec2232bb48b4dd26e2c7
pprmint <29925944+pprmint@users.noreply.github.com>
2022-10-02 22:12:22 +0200

    Slightly faster page transition.

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -13,13 +13,13 @@ export default function PageTransition(props: React.PropsWithChildren) {
                 opacity: 1,
                 x: 0,
 				clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-				transition: { duration: 0.5, ease: "circOut" },
+				transition: { duration: 0.4, ease: "circOut" },
 			}}
 			exit={{
                 opacity: 0,
 				x: -20,
 				clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
-				transition: { duration: 0.25, ease: "easeIn" },
+				transition: { duration: 0.2, ease: "easeIn" },
 			}}
 		>
 			{props.children}
