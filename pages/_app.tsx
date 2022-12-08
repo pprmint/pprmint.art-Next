cec20dc03de6dc36f7c9c1e87be53bc8f44df249
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-08 20:27:59 +0100

    Does this even work...?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,12 +26,55 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:site" content="@npprmint" />
 				<meta name="twitter:creator" content="@npprmint" />
+				<noscript>
+					<div className="bg-red-dark3 po-5 font-sans text-white-dark2">
+						<h1 className="font-sans font-bold text-white text-6xl pb-2">
+							JavaScript seems to be disabled.
+						</h1>
+						<p>
+							If you're a fellow NoScript user: Hiya. You need to add this site
+							to the exceptions in order to see anything useful. If you aren't,
+							either some of your browser settings or extensions don't allow
+							JavaScript, or your browser is just too old.
+						</p>
+						<p>
+							Should JavaScript not be your thing, fear not! I'm also on other
+							sites that kinda require JavaScript to work properly:
+						</p>
+						<ul>
+							<li>
+								<a href="https://twitter.com/npprmint" className="text-blue">
+									Twitter
+								</a>
+							</li>
+							<li>
+								<a href="https://youtube.com/@npprmint" className="text-blue">
+									YouTube
+								</a>
+							</li>
+							<li>
+								<a href="https://npprmint.tumblr.com" className="text-blue">
+									Tumblr
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/pprmint" className="text-blue">
+									GitHub
+								</a>
+							</li>
+						</ul>
+						<br />
+						<sub>
+							also don't mind that lonely menu button, it doesn't work rn.
+						</sub>
+					</div>
+				</noscript>
 			</Head>
 			<NextIntlProvider messages={pageProps.messages}>
 				<LazyMotion features={domAnimation}>
-				<Navigation />
+					<Navigation />
 					<AnimatePresence
-						exitBeforeEnter
+						mode="wait"
 						onExitComplete={() => window.scrollTo(0, 0)}
 					>
 						<m.div
