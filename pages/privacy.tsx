866c37cc177295d2d55ab8fa3ca8557914b3d717
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-08 21:10:31 +0100

    Cleaner use of classes and fixed display of links.

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -47,220 +47,215 @@ export default function PrivacyPolicy() {
 				description={t("Head.description")}
 				ogImg="privacy.png"
 			/>
-			<m.div
-				variants={SectionContainer}
-				initial="hidden"
-				animate="show"
-				exit="exit"
-				className="py-40 max-w-6xl mx-auto px-6"
-			>
-				<m.div variants={Section} className="py-5">
-					<h1 className="font-sans font-bold text-white text-6xl pb-2">
-						{t("Title.bottom")}
-					</h1>
-					<h2 className="font-sans text-white-dark2 text-4xl">
-						{t(
-							"Title.top",
-							{ revisionDate: parseISO("2022-12-07T") } // YYYY-MM-DD
-						)}
-					</h2>
-				</m.div>
+			<main className="py-40 max-w-6xl mx-auto px-6 md:px-9 font-sans text-white-dark2">
+				<m.div
+					variants={SectionContainer}
+					initial="hidden"
+					animate="show"
+					exit="exit"
+				>
+					<m.div variants={Section} className="py-5">
+						<h1 className="font-bold text-white text-5xl md:text-6xl pb-3">
+							{t("Title.bottom")}
+						</h1>
+						<h2 className="text-white-dark2 text-3xl md:text-4xl">
+							{t(
+								"Title.top",
+								{ revisionDate: parseISO("2022-12-07T") } // YYYY-MM-DD
+							)}
+						</h2>
+					</m.div>
 
-				<m.div variants={Section} className="py-5">
-					<div className="bg-gradient-to-br from-black-light1 to-black border-2 border-black-light2 rounded-xl p-6">
-						<h3 className="font-sans font-bold text-white text-3xl pb-2">
-							{t("Content.Tldr.heading")}
+					<m.div variants={Section} className="py-5">
+						<div className="bg-gradient-to-br from-black-light1 to-black border-2 border-black-light2 rounded-xl p-6">
+							<h3 className="font-bold text-white text-3xl pb-3">
+								{t("Content.Tldr.heading")}
+							</h3>
+							<p>{t("Content.Tldr.text")}</p>
+						</div>
+					</m.div>
+
+					<m.div variants={Section} className="py-5">
+						<h3 className="font-bold text-white text-3xl pb-3">
+							{t("Content.General.heading")}
 						</h3>
-						<p className="font-sans text-white-dark2">
-							{t("Content.Tldr.text")}
-						</p>
-					</div>
-				</m.div>
-
-				<m.div variants={Section} className="py-5">
-					<h3 className="font-sans font-bold text-white text-3xl pb-2">
-						{t("Content.General.heading")}
-					</h3>
-					<p className="font-sans text-white-dark2">
-						{t("Content.General.text1")}
-						<br />
-						{t("Content.General.text2")}
-						<br />
-						<Link
-							href=""
-							className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-							scroll={false}
-						>
-							{t.rich("Content.General.example", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-							<ArrowUpRight weight="bold" />
-						</Link>
-					</p>
-				</m.div>
-
-				<m.div variants={Section} className="py-5">
-					<h3 className="font-sans font-bold text-white text-3xl pb-2">
-						{t("Content.Hosting.heading")}
-					</h3>
-					<p className="font-sans text-white-dark2">
-						{t("Content.Hosting.text1")}
-					</p>
-					<br />
-					<p className="font-sans text-white-dark2">
-						{t.rich("Content.Hosting.text2", {
-							strong: (children) => <strong>{children}</strong>,
-						})}
-					</p>
-                    <br />
-					<p className="font-sans text-white-dark2">
-						{t.rich("Content.Hosting.text3", {
-							strong: (children) => <strong>{children}</strong>,
-						})}
-					</p>
-					<br />
-                    <p className="font-sans text-white-dark2">
-						{t("Content.Hosting.text4")}
-					</p>
-                    <br />
-					<div className="pl-3">
-						<p className="font-sans text-white-dark2">
-							{t.rich("Content.Hosting.Log.domain", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-						</p>
-						<br />
-						<p className="font-sans text-white-dark2">
-							{t.rich("Content.Hosting.Log.ipAddress", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-						</p>
-						<br />
-						<p className="font-sans text-white-dark2">
-							{t.rich("Content.Hosting.Log.accessTime", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-						</p>
-						<br />
-						<p className="font-sans text-white-dark2">
-							{t.rich("Content.Hosting.Log.receivedFile", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-						</p>
-						<br />
-						<p className="font-sans text-white-dark2">
-							{t.rich("Content.Hosting.Log.httpCode", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-						</p>
-						<p className="font-sans text-white-dark2">
+						<p>
+							{t("Content.General.text1")}
+							<br />
+							{t("Content.General.text2")}
+							<br />
 							<Link
-								className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-								href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
+								href=""
+								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+								scroll={false}
+							>
+								{t.rich("Content.General.example", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+								<ArrowUpRight weight="bold" className="inline" />
+							</Link>
+						</p>
+					</m.div>
+
+					<m.div variants={Section} className="py-5">
+						<h3 className="font-bold text-white text-3xl pb-3">
+							{t("Content.Hosting.heading")}
+						</h3>
+						<p>{t("Content.Hosting.text1")}</p>
+						<br />
+						<p>
+							{t.rich("Content.Hosting.text2", {
+								strong: (children) => <strong>{children}</strong>,
+							})}
+						</p>
+						<br />
+						<p>
+							{t.rich("Content.Hosting.text3", {
+								strong: (children) => <strong>{children}</strong>,
+							})}
+						</p>
+						<br />
+						<p>{t("Content.Hosting.text4")}</p>
+						<br />
+						<div className="pl-3">
+							<p>
+								{t.rich("Content.Hosting.Log.domain", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+							</p>
+							<br />
+							<p>
+								{t.rich("Content.Hosting.Log.ipAddress", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+							</p>
+							<br />
+							<p>
+								{t.rich("Content.Hosting.Log.accessTime", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+							</p>
+							<br />
+							<p>
+								{t.rich("Content.Hosting.Log.receivedFile", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+							</p>
+							<br />
+							<p>
+								{t.rich("Content.Hosting.Log.httpCode", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+							</p>
+							<p>
+								<Link
+									className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+									href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									HTTP response status codes - HTTP | MDN
+									<ArrowUpRight weight="bold" className="inline" />
+								</Link>
+							</p>
+
+							<p>
+								<br />
+								{t.rich("Content.Hosting.Log.userAgent", {
+									strong: (children) => <strong>{children}</strong>,
+								})}
+								<br />
+							</p>
+							<p>
+								<Link
+									className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+									href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									User-Agent - HTTP | MDN
+									<ArrowUpRight weight="bold" className="inline" />
+								</Link>
+							</p>
+						</div>
+						<br />
+						<p>
+							{t("Content.Hosting.text5")}
+							<br />
+						</p>
+						<p>
+							{t("Content.Hosting.text6")}
+							<br />
+						</p>
+						<p>
+							<Link
+								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+								href="https://vercel.com/legal/privacy-policy"
 								target="_blank"
 								rel="noopener noreferrer"
 							>
-								HTTP response status codes - HTTP | MDN
-								<ArrowUpRight weight="bold" />
+									{t("Content.privacyPolicyOf", {
+										provider: "Vercel",
+									})}
+									<ArrowUpRight weight="bold" className="inline" />
 							</Link>
 						</p>
-
-						<p className="font-sans text-white-dark2">
-							<br />
-							{t.rich("Content.Hosting.Log.userAgent", {
-								strong: (children) => <strong>{children}</strong>,
-							})}
-							<br />
-						</p>
-						<p className="font-sans text-white-dark2">
+						<p>
 							<Link
-								className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-								href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent"
+								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+								href="https://vercel.com/legal/dpa"
 								target="_blank"
 								rel="noopener noreferrer"
 							>
-								User-Agent - HTTP | MDN
-								<ArrowUpRight weight="bold" />
+									{t("Content.dpaOf", {
+										provider: "Vercel",
+									})}
+									<ArrowUpRight weight="bold" className="inline" />
 							</Link>
 						</p>
-					</div>
-					<br />
-					<p className="font-sans text-white-dark2">
-						{t("Content.Hosting.text5")}
-						<br />
-					</p>
-					<p className="font-sans text-white-dark2">
-						{t("Content.Hosting.text6")}
-						<br />
-					</p>
-					<p className="font-sans text-white-dark2">
-						<Link
-							className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-							href="https://vercel.com/legal/privacy-policy"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("Content.privacyPolicyOf", {
-								provider: "Vercel",
-							})}
-							<ArrowUpRight weight="bold" />
-						</Link>
-					</p>
-					<p className="font-sans text-white-dark2">
-						<Link
-							className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-							href="https://vercel.com/legal/dpa"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("Content.dpaOf", {
-								provider: "Vercel",
-							})}
-							<ArrowUpRight weight="bold" />
-						</Link>
-					</p>
-					<p className="font-sans text-white-dark2">
-						<Link
-							className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-							href="https://www.hetzner.com/legal/privacy-policy"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("Content.privacyPolicyOf", {
-								provider: "Hetzner",
-							})}
-							<ArrowUpRight weight="bold" />
-						</Link>
-					</p>
-				</m.div>
+						<p>
+							<Link
+								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+								href="https://www.hetzner.com/legal/privacy-policy"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+									{t("Content.privacyPolicyOf", {
+										provider: "Hetzner",
+									})}
+									<ArrowUpRight weight="bold" className="inline" />
+							</Link>
+						</p>
+					</m.div>
 
-				<m.div variants={Section} className="py-5">
-					<h3 className="font-sans font-bold text-white text-3xl pb-2">
-						{t("Content.Contact.heading")}
-					</h3>
-					<p className="font-sans text-white-dark2">
-						{t.rich("Content.Contact.text1", {
-							strong: (children) => <strong>{children}</strong>,
-						})}
-						<br />
-						{t("Content.Contact.text2")}
-						<br />
-						{t.rich("Content.Contact.text3", {
-							strong: (children) => <strong>{children}</strong>,
-						})}
-						<br />
-						<Link
-							className="flex items-end font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
-							href="https://www.fastmail.com/about/privacy/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{t("Content.privacyPolicyOf", { provider: "Fastmail" })}
-							<ArrowUpRight weight="bold" />
-						</Link>
-					</p>
+					<m.div variants={Section} className="py-5">
+						<h3 className="font-bold text-white text-3xl pb-3">
+							{t("Content.Contact.heading")}
+						</h3>
+						<p>
+							{t.rich("Content.Contact.text1", {
+								strong: (children) => <strong>{children}</strong>,
+							})}
+							<br />
+							{t("Content.Contact.text2")}
+							<br />
+							{t.rich("Content.Contact.text3", {
+								strong: (children) => <strong>{children}</strong>,
+							})}
+							<br />
+							<Link
+								className="font-medium text-blue underline decoration-dotted decoration-2 decoration-blue-dark3 hover:decoration-blue"
+								href="https://www.fastmail.com/about/privacy/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+									{t("Content.privacyPolicyOf", { provider: "Fastmail" })}
+									<ArrowUpRight weight="bold" className="inline" />
+							</Link>
+						</p>
+					</m.div>
 				</m.div>
-			</m.div>
+			</main>
 		</>
 	);
 }
