05fa2d80b55f5f94f1f1e22b19a2fa0494991006
Nicklas <29925944+pprmint@users.noreply.github.com>
2022-12-09 10:09:46 +0000

    Add slider to soon change column size in gallery.

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { GetStaticPropsContext } from "next";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import Image from "next/image";
 import { m } from "framer-motion";
 
 import Head from "components/Head";
 import { ArrowsOut, Article } from "phosphor-react";
-import Link from "next/link";
+import StyledSlider from "components/Slider";
 
 const SectionContainer = {
 	hidden: { opacity: 0 },
@@ -43,7 +44,8 @@ const Works2022 = [
 	{
 		caption: "Blossom",
 		src: "https://static.pprmint.art/works/2022/Blossom/3D_Blossom_alt_Post2.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOscA35DwAEYAISA/VyngAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOscA35DwAEYAISA/VyngAAAABJRU5ErkJggg==",
 		width: 3840,
 		height: 2160,
 		tumblr: "https://blog.pprmint.art/post/701659309102202880/awfiquily-rev-2",
@@ -51,7 +53,8 @@ const Works2022 = [
 	{
 		caption: "Purple Quartz",
 		src: "https://static.pprmint.art/works/2022/PurpleQuartz/Purple_Quartz.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOMta/+DwAEMAIYH14umAAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOMta/+DwAEMAIYH14umAAAAABJRU5ErkJggg==",
 		width: 3840,
 		height: 2160,
 		tumblr: "https://blog.pprmint.art/post/701202293755953152/purple-quartz",
@@ -59,133 +62,152 @@ const Works2022 = [
 	{
 		caption: "Landscape",
 		src: "https://static.pprmint.art/works/2022/Landscape/Landscape.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMsKan8DwAFJwJi2lLHfwAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMsKan8DwAFJwJi2lLHfwAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Pimples",
 		src: "https://static.pprmint.art/works/2022/Pimples/Confusing_POST.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUEhL4DwABpwE1wrSuHQAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUEhL4DwABpwE1wrSuHQAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Pyramid",
 		src: "https://static.pprmint.art/works/2022/Pyramid/pyramid.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO0Vyn+DwADVwHXFVegmgAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO0Vyn+DwADVwHXFVegmgAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Platonic",
 		src: "https://static.pprmint.art/works/2022/Platonic/platonic_edit_mirrored.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUNFb+DwACLAFoF1fa3wAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUNFb+DwACLAFoF1fa3wAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Next.js",
 		src: "https://static.pprmint.art/works/2022/Nextjs/NextJS.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkkPP/DwACAQFuxXoefQAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkkPP/DwACAQFuxXoefQAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "what",
 		src: "https://static.pprmint.art/works/2022/What/whatb.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMsrTzwHwAFyAKvBZZt1gAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMsrTzwHwAFyAKvBZZt1gAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Solar System 2",
 		src: "https://static.pprmint.art/works/2022/SolarSystem/Solar_System_2.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN0ctT/DwADMgGzY1TPcgAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN0ctT/DwADMgGzY1TPcgAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Flap",
 		src: "https://static.pprmint.art/works/2022/Flap/Flap.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNsPPvvPwAHcANNlsBOAgAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNsPPvvPwAHcANNlsBOAgAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Swirl",
 		src: "https://static.pprmint.art/works/2022/Swirl/Swirl_1080p_E.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMs95z4HwAE4gJSNSIlJwAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMs95z4HwAE4gJSNSIlJwAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Lights",
 		src: "https://static.pprmint.art/works/2022/Lights/N-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkUrD4DwACAQFjqUq7jwAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkUrD4DwACAQFjqUq7jwAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Book",
 		src: "https://static.pprmint.art/works/2022/Book/Cover-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUFhL4DwABqwE2D0tXzwAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUFhL4DwABqwE2D0tXzwAAAABJRU5ErkJggg==",
 		width: 2481,
 		height: 3508,
 	},
 	{
 		caption: "MINT",
 		src: "https://static.pprmint.art/works/2022/MintBanners/MINT_Night-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMU/A8AAScBEsBwhbUAAAAASUVORK5CYII=",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMU/A8AAScBEsBwhbUAAAAASUVORK5CYII=",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Wii remake",
 		src: "https://static.pprmint.art/works/2022/WiiRemake/WiiRemake-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8++fPfwAJ6QP2LpIhtgAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8++fPfwAJ6QP2LpIhtgAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Geoballs",
 		src: "https://static.pprmint.art/works/2022/Geoices/geoballs-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNUlXf8DwACfAGG83/Z/gAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNUlXf8DwACfAGG83/Z/gAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "SUSE rebrand concept",
 		src: "https://static.pprmint.art/works/2022/SUSE/suse-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0t7H/DwADFwGzqTOkAQAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0t7H/DwADFwGzqTOkAQAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Statistics",
 		src: "https://static.pprmint.art/works/2022/Statistics/statistics-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMU+g8AASkBE945rRMAAAAASUVORK5CYII=",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMU+g8AASkBE945rRMAAAAASUVORK5CYII=",
 		width: 2000,
 		height: 2000,
 	},
 	{
 		caption: "iPad",
 		src: "https://static.pprmint.art/works/2022/iPad/iPad-720.png",
-        blurData: "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNU+Q8AAU0BJUYogwYAAAAASUVORK5CYII=",
+		blurData:
+			"data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNU+Q8AAU0BJUYogwYAAAAASUVORK5CYII=",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Arch wallpaper",
 		src: "https://static.pprmint.art/works/2022/ArchWall/Arch_Qogir-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUkVX8DwACEgFb11SrOwAAAABJRU5ErkJggg==",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUkVX8DwACEgFb11SrOwAAAABJRU5ErkJggg==",
 		width: 1920,
 		height: 1080,
 	},
 	{
 		caption: "Ford rebrand concept",
 		src: "https://static.pprmint.art/works/2022/Ford/Ford-720.png",
-        blurData: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=",
+		blurData:
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=",
 		width: 1920,
 		height: 1080,
 	},
@@ -193,6 +215,7 @@ const Works2022 = [
 
 export default function Gallery() {
 	const t = useTranslations("Gallery");
+	const cols = React.useState(3);
 	return (
 		<>
 			<Head
@@ -208,10 +231,20 @@ export default function Gallery() {
 					exit="exit"
 				>
 					<m.div variants={Section} className="py-5">
-						<section className="max-w-6xl mx-auto px-6 md:px-9">
-							<h1 className="font-bold text-white text-5xl md:text-6xl pb-3">
+						<section className="px-6 md:px-9 flex items-center flex-col md:flex-row">
+							<h1 className="font-bold text-white text-5xl md:text-6xl pb-3 flex-grow">
 								{t("Head.title")}
 							</h1>
+							<div className="flex flex-col w-full md:max-w-xl">
+                                <span className="mb-3">{t("Content.columnCount")}</span>
+								<StyledSlider
+									label="Columns"
+									min={1}
+									default={[3]}
+									max={6}
+									step={1}
+								/>
+							</div>
 						</section>
 					</m.div>
 					<section className="py-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
@@ -258,8 +291,8 @@ export default function Gallery() {
 									width={Work.width}
 									height={Work.height}
 									alt={Work.caption}
-                                    placeholder="blur"
-                                    blurDataURL={Work.blurData}
+									placeholder="blur"
+									blurDataURL={Work.blurData}
 									className="h-full object-cover"
 								/>
 							</m.div>
