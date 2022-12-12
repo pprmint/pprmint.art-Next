b63cc9439e59020b6d0bf554dd4faee96fc5a4cb
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-12 21:05:37 +0100

    Fix text sizes and add random strings.

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,6 +7,12 @@ import { m } from "framer-motion";
 import Head from "components/Head";
 import Button from "components/Button";
 import { House } from "phosphor-react";
+import NoSSR from "components/NoSSR";
+
+function randomIntForString(min: number, max: number) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const randomStringInt = randomIntForString(1, 8);
 
 const SectionContainer = {
 	hidden: { opacity: 0 },
@@ -56,14 +62,17 @@ export default function NotFound() {
 				className="py-40 max-w-6xl h-screen px-6 mx-auto flex flex-col justify-center items-center"
 			>
 				<m.div variants={Section}>
-					<h1 className="font-thin text-red text-9xl pb-2 text-center">
-						{t("Content.error")}: <span className="font-normal font-pixel">404</span>
+					<h1 className="font-thin text-red text-7xl md:text-9xl pb-2 text-center">
+						{t("Content.error")}
+						<span className="font-normal font-pixel">404</span>
 					</h1>
 				</m.div>
 				<m.div variants={Section}>
-					<h2 className="font-sans font-bold text-white text-6xl pb-2 text-center">
-						{t("Content.notFound")}
-					</h2>
+					<NoSSR>
+						<h2 className="font-sans font-bold text-white text-5xl md:text-6xl pb-2 text-center">
+							{t("Content.Random." + randomStringInt)}
+						</h2>
+					</NoSSR>
 				</m.div>
 				<m.div variants={Section} className="pb-12">
 					<h3 className="font-sans text-white-dark2 text-2xl text-center">
