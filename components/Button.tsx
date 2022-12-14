f59c309b98693b08996e53419cfec7d247e68be7
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-14 17:55:53 +0100

    Allow color of button to be changed with prop.

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,16 +4,27 @@ import React from "react";
 export default function Button(
 	props: React.PropsWithChildren<{
 		large?: boolean;
+		color?: string;
 	}>
 ) {
 	const size = props.large
 		? "rounded-xl text-xl md:text-2xl pt-3 pb-3 px-6 font-normal hover:font-bold"
 		: "rounded-md pt-1 pb-1.5 px-4 font-normal";
+	const color =
+		props.color === "green"
+			? "bg-green hover:bg-green-dark1 active:bg-green-dark2 text-black"
+			: props.color === "yellow"
+			? "bg-yellow hover:bg-yellow-dark1 active:bg-yellow-dark2 text-black"
+			: props.color === "blue"
+			? "bg-blue hover:bg-blue-dark1 active:bg-blue-dark2 text-black"
+			: props.color === "red"
+			? "bg-red hover:bg-red-dark1 active:bg-red-dark2 text-black"
+			: "bg-white hover:bg-white-dark1 active:bg-white-dark2 text-black";
 	return (
 		<button
 			className={
-				"bg-white hover:bg-white-dark1 active:bg-white-dark2 text-black font-sans duration-150 active:scale-95 flex items-center gap-5 w-max h-min" +
-				" " +
+                color +
+				" font-sans duration-150 active:scale-95 flex items-center gap-5 w-max h-min " +
 				size
 			}
 		>
