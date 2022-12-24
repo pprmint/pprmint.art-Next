8c4fd74326f54015d2e99dc539b120299a73563b
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-24 19:06:10 +0100

    Restructuring.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -83,46 +83,55 @@ const Carret = (
 		aria-hidden
 	/>
 );
-const NavMenuTrigger =
-	"group/root flex items-center gap-3 py-3 px-4 text-white-dark2 hover:text-white data-[state='open']:text-white duration-250 font-medium data-[state='open']:bg-black-light1 rounded-t-md data-[state='open']:translate-y-3";
-const NavMenuContent =
-	"absolute top-0 left-0 p-3 duration-250 data-[motion='from-start']:animate-enter-from-l data-[motion='from-end']:animate-enter-from-r data-[motion='to-start']:animate-exit-to-l data-[motion='to-end']:animate-exit-to-r";
-const NavMenuViewport =
-	"relative origin-top-left w-[var(--radix-navigation-menu-viewport-width)] overflow-hidden bg-black-light1 text-white-dark2 rounded-lg shadow-xl shadow-black h-[var(--radix-navigation-menu-viewport-height)] duration-250 ease-out";
-const NavMenuLink =
-	"block p-3 hover:bg-black-light2 duration-250 ease-out rounded-sm";
-
-const NavAccordionItem =
-	"group/root data-[state='open']:backdrop-brightness-75 duration-250 w-full";
-const NavAccordionTrigger =
-	"flex w-full text-left items-center px-6 py-4 data-[state='open']:py-6 hover:backdrop-brightness-200 data-[state='open']:font-bold text-white text-3xl data-[state='open']:text-4xl duration-250 ease-out";
-const NavAccordionContent =
-	"data-[state='open']:animate-slide-down data-[state='closed']:animate-slide-up overflow-hidden";
-const NavAccordionContentList = "flex flex-col p-6 pt-0 gap-3 text-xl";
-const NavAccordionContentListItem = "hover:text-white duration-100";
-
-function ListItem(
-	props: React.PropsWithChildren<{ href: string; strings: string }>
-) {
-	const t = useTranslations();
-	return (
-		<li>
-			<NavigationMenu.Link asChild className={NavMenuLink}>
-				<Link href={props.href}>
-					<span className="text-white font-display font-bold text-xl">
-						{t(props.strings + ".title")}
-					</span>
-					<p className="ListItemText">{t(props.strings + ".description")}</p>
-				</Link>
-			</NavigationMenu.Link>
-		</li>
-	);
-}
 
 function DesktopNavigation() {
 	const t = useTranslations();
+
+	// Language switch
 	const { locale, locales, route } = useRouter();
 	const otherLocale = locales?.find((cur) => cur !== locale);
+
+	// Styles
+	const NavMenuTrigger =
+		"group/root flex items-center gap-3 py-3 px-4 text-white-dark2 hover:text-white data-[state='open']:text-white duration-250 font-medium data-[state='open']:bg-black-light1 rounded-t-md data-[state='open']:translate-y-3";
+	const NavMenuContent =
+		"absolute top-0 left-0 p-3 duration-250 data-[motion='from-start']:animate-enter-from-l data-[motion='from-end']:animate-enter-from-r data-[motion='to-start']:animate-exit-to-l data-[motion='to-end']:animate-exit-to-r";
+	const NavMenuViewport =
+		"relative origin-top-left w-[var(--radix-navigation-menu-viewport-width)] overflow-hidden bg-black-light1 text-white-dark2 rounded-lg shadow-xl shadow-black h-[var(--radix-navigation-menu-viewport-height)] duration-250 ease-out";
+
+	function MenuItem(
+		props: React.PropsWithChildren<{
+			href: string;
+			title: string;
+			description: string;
+			locale?: string;
+			disableHighlight?: boolean;
+		}>
+	) {
+		const highlight =
+			!props.disableHighlight &&
+			props.href === route &&
+			" bg-black-light2 pointer-events-none";
+		return (
+			<li>
+				<NavigationMenu.Link
+					asChild
+					className={
+						"block p-3 hover:bg-black-light2 duration-250 ease-out rounded-sm" +
+						highlight
+					}
+				>
+					<Link href={props.href} scroll={false} locale={props.locale}>
+						<span className="text-white font-display font-bold text-xl pb-0.5">
+							{props.title}
+						</span>
+						<p className="ListItemText">{props.description}</p>
+					</Link>
+				</NavigationMenu.Link>
+			</li>
+		);
+	}
+
 	return (
 		<>
 			<NavigationMenu.Root>
@@ -138,10 +147,11 @@ function DesktopNavigation() {
 						>
 							<ul className="grid grid-flow-row grid-cols-2">
 								{Pages.map((Page) => (
-									<ListItem
+									<MenuItem
 										key={Page.link}
 										href={Page.link}
-										strings={Page.strings}
+										title={t(Page.strings + ".title")}
+										description={t(Page.strings + ".description")}
 									/>
 								))}
 							</ul>
@@ -159,10 +169,11 @@ function DesktopNavigation() {
 						>
 							<ul className="grid grid-cols-2 grid-flow-row">
 								{Projects.map((Project) => (
-									<ListItem
+									<MenuItem
 										key={Project.link}
 										href={Project.link}
-										strings={Project.strings}
+										title={t(Project.strings + ".title")}
+										description={t(Project.strings + ".description")}
 									/>
 								))}
 							</ul>
@@ -179,36 +190,20 @@ function DesktopNavigation() {
 							className={NavMenuContent + " w-[400px] lg:w-[500px]"}
 						>
 							<ul className="grid grid-flow-row">
-								<li>
-									<NavigationMenu.Link asChild>
-										<Link href="/privacy" className={NavMenuLink}>
-											<span className="text-white font-display font-bold text-xl">
-												{t("PrivacyPolicy.Head.title")}
-											</span>
-											<p className="ListItemText">
-												{t("PrivacyPolicy.Head.description")}
-											</p>
-										</Link>
-									</NavigationMenu.Link>
-								</li>
-								<li>
-									<NavigationMenu.Link asChild>
-										<Link
-											href={route}
-											locale={otherLocale}
-											scroll={false}
-											className={NavMenuLink}
-										>
-											<span className="text-white font-display font-bold text-xl">
-												{t("Navigation.SwitchLocale.title")}
-											</span>
-											<p className="ListItemText">
-												{t("Navigation.SwitchLocale.description")}
-											</p>
-										</Link>
-									</NavigationMenu.Link>
-								</li>
+								<MenuItem
+									title={t("PrivacyPolicy.Head.title")}
+									description={t("PrivacyPolicy.Head.description")}
+									href="/privacy"
+								/>
+								<MenuItem
+									title={t("Navigation.SwitchLocale.title")}
+									description={t("Navigation.SwitchLocale.description")}
+									href={route}
+									locale={otherLocale}
+									disableHighlight
+								/>
 							</ul>
+							<Footer className="pl-3 pt-3 items-center " />
 						</NavigationMenu.Content>
 					</NavigationMenu.Item>
 				</NavigationMenu.List>
@@ -222,8 +217,12 @@ function DesktopNavigation() {
 
 function MobileNavigation() {
 	const t = useTranslations();
+
+	// Language switch
 	const { locale, locales, route } = useRouter();
 	const otherLocale = locales?.find((cur) => cur !== locale);
+
+	// Opening and closing logic
 	const [open, setOpen] = React.useState(false);
 	const handleOpen = () => {
 		setOpen(true);
@@ -235,6 +234,42 @@ function MobileNavigation() {
 		document.body.classList.remove("overflow-hidden");
 	};
 	const toggleOpen = open ? handleClose : handleOpen;
+
+	// Styles
+	const NavAccordionItem =
+		"group/root data-[state='open']:backdrop-brightness-75 duration-250 w-full border-black-light2 border-0 data-[state='open']:border-y";
+	const NavAccordionTrigger =
+		"flex w-full text-left items-center px-6 py-4 data-[state='open']:py-6 hover:backdrop-brightness-200 data-[state='open']:font-bold text-white text-3xl data-[state='open']:text-4xl duration-250 ease-out";
+	const NavAccordionContent =
+		"data-[state='open']:animate-slide-down data-[state='closed']:animate-slide-up overflow-hidden";
+	const NavAccordionContentList = "flex flex-col p-6 pt-0 text-xl";
+
+	function ListItem(
+		props: React.PropsWithChildren<{
+			href: string;
+			title: string;
+			locale?: string;
+			disableHighlight?: boolean;
+		}>
+	) {
+		const highlight =
+			!props.disableHighlight &&
+			props.href === route &&
+			" font-bold text-white pointer-events-none";
+		return (
+			<Link
+				href={props.href}
+				scroll={false}
+				onClick={() => setOpen(false)}
+				locale={props.locale}
+			>
+				<li className={"hover:text-white py-1.5 duration-100" + highlight}>
+					{props.title}
+				</li>
+			</Link>
+		);
+	}
+
 	return (
 		<>
 			<button
@@ -245,30 +280,35 @@ function MobileNavigation() {
 			</button>
 			<AnimatePresence>
 				{open && (
-					<Portal.Root>
+					<Portal.Root className="flex">
+                        {/* Overlay */}
 						<m.div
 							initial={{ opacity: 0 }}
-							animate={{ opacity: 0.7 }}
-							exit={{ opacity: 0 }}
-							transition={{ duration: 0.2 }}
-							className="fixed top-0 left-0 bg-black w-screen h-screen z-30"
-							onClick={toggleOpen}
+							animate={{ opacity: 1, transition: { duration: 0.2 } }}
+							exit={{ opacity: 0, transition: { duration: 0.2, delay: 0.2 } }}
+							className="fixed top-0 left-0 bg-[#111a] w-screen h-screen z-40 backdrop-blur-md"
+							onClick={handleClose}
 						/>
+                        {/* Main container */}
 						<m.div
-							initial={{ opacity: 0, y: 50 }}
+							initial={{ opacity: 0, y: "calc(-50% + 50px)" }}
 							animate={{
 								opacity: 1,
-								y: 0,
-								transition: { duration: 0.4, ease: "circOut" },
+								y: "-50%",
+								transition: { duration: 0.4, ease: "circOut", delay: 0.2 },
 							}}
 							exit={{
 								opacity: 0,
-								y: 20,
+								y: "calc(-50% + 50px)",
 								transition: { duration: 0.2, ease: "easeIn" },
 							}}
-							className="fixed top-0 left-0 flex flex-col w-full h-full z-40 text-white-dark2 backdrop-blur-md justify-center"
+							className="z-40 absolute -translate-y-1/2 top-1/2 w-full"
 						>
-							<Accordion.Root type="single" collapsible className="w-full">
+							<Accordion.Root
+								type="single"
+								collapsible
+								className="w-full text-white-dark2"
+							>
 								<Accordion.Item value="general" className={NavAccordionItem}>
 									<Accordion.Header>
 										<Accordion.Trigger className={NavAccordionTrigger}>
@@ -279,19 +319,11 @@ function MobileNavigation() {
 									<Accordion.Content className={NavAccordionContent}>
 										<ul className={NavAccordionContentList}>
 											{Pages.map((Page) => (
-												<Link
+												<ListItem
 													key={Page.link}
+													title={t(Page.strings + ".title")}
 													href={Page.link}
-													scroll={false}
-													onClick={() => setOpen(false)}
-												>
-													<li
-														className={NavAccordionContentListItem}
-														key={Page.link}
-													>
-														{t(Page.strings + ".title")}
-													</li>
-												</Link>
+												/>
 											))}
 										</ul>
 									</Accordion.Content>
@@ -307,19 +339,11 @@ function MobileNavigation() {
 									<Accordion.Content className={NavAccordionContent}>
 										<ul className={NavAccordionContentList}>
 											{Projects.map((Project) => (
-												<Link
+												<ListItem
 													key={Project.link}
+													title={t(Project.strings + ".title")}
 													href={Project.link}
-													scroll={false}
-													onClick={() => setOpen(false)}
-												>
-													<li
-														className={NavAccordionContentListItem}
-														key={Project.link}
-													>
-														{t(Project.strings + ".title")}
-													</li>
-												</Link>
+												/>
 											))}
 										</ul>
 									</Accordion.Content>
@@ -334,25 +358,37 @@ function MobileNavigation() {
 									</Accordion.Header>
 									<Accordion.Content className={NavAccordionContent}>
 										<ul className={NavAccordionContentList}>
-											<Link
+											<ListItem
 												href="/privacy"
-												scroll={false}
-												onClick={() => setOpen(false)}
-											>
-												<li className={NavAccordionContentListItem}>
-													{t("PrivacyPolicy.Head.title")}
-												</li>
-											</Link>
-											<Link href={route} locale={otherLocale} scroll={false}>
-												<li className={NavAccordionContentListItem}>
-													{t("Navigation.SwitchLocale.title")}
-												</li>
-											</Link>
+												title={t("PrivacyPolicy.Head.title")}
+											/>
+											<ListItem
+												href={route}
+												locale={otherLocale}
+												title={t("Navigation.SwitchLocale.title")}
+												disableHighlight
+											/>
 										</ul>
 									</Accordion.Content>
 								</Accordion.Item>
 							</Accordion.Root>
-							<Footer />
+						</m.div>
+                        {/* Footer */}
+						<m.div
+							initial={{ opacity: 0, y: 50 }}
+							animate={{
+								opacity: 1,
+								y: 0,
+								transition: { duration: 0.4, ease: "circOut", delay: 0.3 },
+							}}
+							exit={{
+								opacity: 0,
+								y: 20,
+								transition: { duration: 0.2, ease: "easeIn" },
+							}}
+							className="absolute bottom-0 p-6 w-full z-40"
+						>
+							<Footer className="items-end" />
 						</m.div>
 					</Portal.Root>
 				)}
@@ -362,10 +398,10 @@ function MobileNavigation() {
 }
 
 // Made with <3 and coffee + social buttons
-function Footer() {
+function Footer(props: { className?: string }) {
 	const t = useTranslations("Navigation");
 	return (
-		<div className="flex absolute p-6 w-full flex-row items-end bottom-0">
+		<div className={"flex flex-row " + props.className}>
 			<div className="text-white-dark2 text-xs flex-grow">
 				<p className="flex flex-row items-center leading-3">
 					{t("madeWith")}
