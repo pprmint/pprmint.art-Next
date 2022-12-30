e4f924298985eba302587ba8a535e95bbedb3139
pprmint <29925944+pprmint@users.noreply.github.com>
2022-12-30 09:37:37 +0100

    Add noscript warning to fixed div.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,51 +28,54 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name="twitter:site" content="@npprmint" />
 				<meta name="twitter:creator" content="@npprmint" />
 				<noscript>
-					<div className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-5xl w-4/5 rounded-lg bg-black-light1 font-sans text-white-dark2">
-						<div className="bg-red-dark3 rounded-t-lg">
-							<h1 className="font-sans font-bold text-white text-4xl md:text-5xl pb-2 p-5 md:p-10 ">
-								JavaScript seems to be disabled.
-							</h1>
-						</div>
-						<div className="p-5 md:p-10">
-							<p>
-								If you're a fellow NoScript user: Hiya. You need to add this
-								site to the exceptions in order to see anything useful. If you
-								aren't, either some of your browser settings or extensions don't
-								allow JavaScript, or your browser is just too old.
-							</p>
-							<br />
-							<p>
-								Should JavaScript not be your thing, fear not! I'm also on other
-								sites that kinda require JavaScript to work properly:
-							</p>
-							<ul className="list-disc list-inside">
-								<li>
-									<a href="https://twitter.com/npprmint" className="text-blue">
-										Twitter
-									</a>
-								</li>
-								<li>
-									<a href="https://youtube.com/@npprmint" className="text-blue">
-										YouTube
-									</a>
-								</li>
-								<li>
-									<a href="https://npprmint.tumblr.com" className="text-blue">
-										Tumblr
-									</a>
-								</li>
-								<li>
-									<a href="https://github.com/pprmint" className="text-blue">
-										GitHub
-									</a>
-								</li>
-							</ul>
-							<br />
-							<sub>
-								also don't mind that lonely menu button, it doesn't work rn. cuz
-								javascript. go figure.
-							</sub>
+					<div className="bg-black fixed z-40 w-screen h-screen">
+						<div className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-5xl w-4/5 rounded-lg bg-black-light1 font-sans text-white-dark2">
+							<div className="bg-red-dark3 rounded-t-lg">
+								<h1 className="font-sans font-bold text-white text-3xl md:text-5xl p-5 md:p-10 ">
+									JavaScript seems to be disabled.
+								</h1>
+							</div>
+							<div className="p-5 md:p-10">
+								<p>
+									Hiya. This site uses a lot of fancy animations and features
+									that require JavaScript to work (for example, navigation menus
+									and pop-ups are added and removed from the DOM tree as
+									needed).
+								</p>
+								<br />
+								<p>
+									Should JavaScript not be your thing, fear not! I'm also on
+									other sites that kinda require JavaScript to work properly:
+								</p>
+								<ul className="list-disc list-inside">
+									<li>
+										<a
+											href="https://twitter.com/npprmint"
+											className="text-blue"
+										>
+											Twitter
+										</a>
+									</li>
+									<li>
+										<a
+											href="https://youtube.com/@npprmint"
+											className="text-blue"
+										>
+											YouTube
+										</a>
+									</li>
+									<li>
+										<a href="https://npprmint.tumblr.com" className="text-blue">
+											Tumblr
+										</a>
+									</li>
+									<li>
+										<a href="https://github.com/pprmint" className="text-blue">
+											GitHub
+										</a>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</noscript>
@@ -85,7 +88,7 @@ export default function App({ Component, pageProps }: AppProps) {
 						onExitComplete={() => window.scrollTo(0, 0)}
 					>
 						<m.div
-                            key={location}
+							key={location}
 							initial={{ opacity: 0, y: -50 }}
 							animate={{
 								opacity: 1,
